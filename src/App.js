5a162f8b8ace53ece046c3b1ed1aa3cee7f07a95
“Olaifa-Olawale” <olawale.favour10@gmail.com>
2023-12-20 17:56:01 -0600

    End Of Login Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
 import './App.css';
+import Login from './components/Login';
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-        <h2>test</h2>
+      <Router>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/home">
+              <Header />
+              <Home />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
